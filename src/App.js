5cb93a3fd5f9f1daa9104e330bbb548b1eba9968
faisalsayed10 <faisal.sayed502@gmail.com>
2020-09-05 12:11:25 +0530

    added balance and clear form functioning

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import Blocks from "./Blocks";
 import AddInputs from "./AddInputs";
 
 function App() {
-  const [income, setIncome] = useState([]);
-  const [expense, setExpense] = useState([]);
+  const [incomes, setIncomes] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+  const [limit, setLimit] = useState("0");
+  const [error, setError] = useState('')
 
   return (
     <div className="text-center">
       <header>
-        <h1 className="text-white text-5xl bg-gray-800 p-1 mb-5">Expense Tracker</h1>
+        <h1 className="text-white text-5xl bg-gray-800 p-1 mb-5">
+          Expense Tracker
+        </h1>
       </header>
-      <Blocks income={income} expense={expense} />
+      <Blocks incomes={incomes} expenses={expenses} />
       <AddInputs
-        income={income}
-        expense={expense}
-        setIncome={setIncome}
-        setExpense={setExpense}
+        setIncomes={setIncomes}
+        setExpenses={setExpenses}
+        setLimit={setLimit}
+        limit={limit}
+        error={error}
+        setError={setError}
       />
     </div>
   );
--- src/Blocks.js
+++ src/Blocks.js
@@ -1,31 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const blockClass = "rounded-lg shadow-2xl p-4"
+const blockClass = "rounded-lg shadow-2xl p-4";
 
-function Blocks({ income, expense }) {
+let totalExpense = 0;
+let totalIncome = 0;
+let incomeAmtArr = [];
+let expenseAmtArr = [];
 
-  let totalExpense = 0;
-  if (expense.length > 0) {
-    totalExpense = expense.reduce((a, b) => a + parseInt(b), 0);
-    console.log(totalExpense)
+function Blocks({ incomes, expenses }) {
+  const [balance, setBalance] = useState(0);
+
+  console.log({ balance, incomes, expenses });
+
+  if (expenses.length > 0) {
+    //Extracting the amount from the object
+    expenseAmtArr = expenses.map((expense) => expense.expense);
+    //Summing up the amount
+    totalExpense = expenseAmtArr.reduce((a, b) => parseInt(a) + parseInt(b));
   }
 
-  let totalIncome = 0;
-  if (income.length > 0) {
-    totalIncome = income.reduce((a, b) => a + parseInt(b), 0);
-    console.log(totalIncome)
+  if (incomes.length > 0) {
+    //Extracting the amount from the object
+    incomeAmtArr = incomes.map((income) => income.income);
+    //Summing up the amount
+    totalIncome = incomeAmtArr.reduce((a, b) => parseInt(a) + parseInt(b));
   }
 
+  useEffect(() => {
+    let remainder = totalIncome - totalExpense;
+
+    if (incomeAmtArr.length >= 0 && expenseAmtArr.length >= 0) {
+      setBalance(remainder);
+    }
+
+  }, [incomes, expenses]);
 
   return (
     <div className="flex justify-evenly">
       <div className={blockClass}>
-        <h1 className="text-3xl w-56">Income</h1>
+        <h2 className="text-3xl w-56">Income</h2>
         <h1 className="text-5xl w-56">${totalIncome}</h1>
       </div>
       <div className={blockClass}>
-        <h1 className="text-3xl w-56">Expenses</h1>
+        <h2 className="text-3xl w-56">Expenses</h2>
         <h1 className="text-5xl w-56">${totalExpense}</h1>
+      </div>
+      <div className={blockClass}>
+        <h2 className="text-3xl w-56">Balance</h2>
+        <h1 className="text-5xl w-56">${balance}</h1>
       </div>
     </div>
   );
